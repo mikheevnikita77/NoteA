8240fbd34f1798efa43863dd62fb35dd59acd955
mikheevnikita77 <mikhevvnikita77@gmail.com>
2019-10-08 10:14:41 +0700

    Добавили комментарий

--- WindowsFormsApp1/WindowsFormsApp1/MainForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.cs
@@ -6,7 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Forms;// система виндос
+
 
 namespace WindowsFormsApp1
 {
